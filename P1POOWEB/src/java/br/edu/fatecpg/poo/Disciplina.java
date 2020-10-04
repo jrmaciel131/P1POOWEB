58f6af73e07ca7050ffb6e6f696168ed0d428d28
César Maciel <62404103+jrmaciel131@users.noreply.github.com>
2020-10-04 06:34:05 -0300

    Class Disciplina

---
+++ P1POOWEB/src/java/br/edu/fatecpg/poo/Disciplina.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fatecpg.poo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jrmac
+ */
+public class Disciplina {
+    private String name, ementa;
+    private int ciclo;
+    private double nota;
+    private ArrayList<Disciplina> disciplinas = new ArrayList<>();
+
+    public Disciplina() {
+    }
+
+    public Disciplina(String name, String ementa, int ciclo) {
+        this.name = name;
+        this.ementa = ementa;
+        this.ciclo = ciclo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public int getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public ArrayList<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(Disciplina disciplinas) {
+        this.disciplinas.add(disciplinas);
+    }
+    
+    public ArrayList<Disciplina> getList(){
+        return disciplinas;
+    }
+    
+    
+    
+    
+    
+}
